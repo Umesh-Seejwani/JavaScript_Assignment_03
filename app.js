2e8bea5e060241c0c857693cc56b068b184245a2
Umesh Premjee <130490125+Umesh-Seejwani@users.noreply.github.com>
2024-02-05 14:47:01 +0500

    Completed

---
+++ app.js
@@ -0,0 +1,138 @@
+// QUESTION NO 1:
+// var city = prompt("Enter City Name :");
+// if(city === "karachi"){
+//     console.log("Welcome to the City Of Lights:");
+// } 
+// else{
+//     console.log("Welcome To " +city);
+// }
+
+
+// QUESTION NO 2:
+// var gender = prompt("Enter Gender :");
+// if(gender === "male"){
+//     console.log("Good Morning Sir:");
+// } 
+// if(gender === "femail"){
+//     console.log("Good Morning Ma'am:");
+//  }
+
+
+// QUESTION NO 3:
+// var fuel = +prompt("Enter Remaining Fuel:");
+
+// if(fuel <= 0.25){
+//     console.log("Please Refill the Fuel in your Car!");
+// }
+// else{
+//     console.log("Enough fuel in your Car!");
+// }
+
+// var a = 4;  
+// if (++a === 5){  
+//     alert("given condition for variable a is true");  }
+
+// var b = 82; 
+//  if (b++ === 83){  
+//     alert("given condition for variable b is true");  
+// } 
+// alert("given condition for variable b is not true");  
+
+// var c = 12;
+//   if (c++ === 13){ 
+//      alert("condition 1 is true");  } 
+//       if (c === 13){  
+//         alert("condition 2 is true");  }  
+//       if (++c < 14){ 
+//          alert("condition 3 is true");  
+// }  if(c === 14){ 
+//      alert("condition 4 is true");  } 
+
+// var materialCost = 20000; 
+//  var laborCost = 2000; 
+//   var totalCost = materialCost + laborCost;  
+//   if (totalCost === laborCost + materialCost){ 
+//     alert("The cost equals");  }
+
+// if (true){ 
+//      alert("True");  } 
+//      if (false){  
+//         alert("False");  }
+
+// if("car" < "cat"){  
+//     alert("car is smaller than cat");  }
+
+// QUESTION NO 5:
+// var secretNumber = 6;
+// var user = +prompt("Guess The Number:");
+
+// if(user === secretNumber){
+//     console.log("Bingo! Correct Answer.");
+    
+// }
+//  else if(user+1 ===secretNumber){
+//     console.log("Close Enough to the Correct Answer.");
+// }
+// else{
+//     console.log("Try Again.");
+// }
+
+// QUESTION NO 6:
+// var num = +prompt("Enter a Number:");
+
+// if(num % 3 === 0){
+//     console.log("The Number is Divisible By 3 " );
+// }
+// else{
+//     console.log("The Number is Not Divisible By 3");
+// }
+
+// QUESTION NO 7:
+// var num = +prompt("Enter a Number:");
+
+// if(num % 2 === 0 ){
+//     console.log("The NUmber is Even " +num);
+// }
+// else{
+//     console.log("The Number is Odd " +num);
+// }
+
+// QUESTION NO 8:
+// var temp = +prompt("Enter Temperature:");
+
+// if(temp > 40){
+//     console.log("It is too Hot Outside.");
+// }
+// else if(temp > 30){
+//     console.log("The Weather today is Normal.");
+// }
+// else if(temp > 20){
+//     console.log("Today’s Weather is cool.");
+// }
+// else if(temp > 10){
+//     console.log("OMG! Todays weather is so Cool.");
+// }
+// else{
+//     console.log("The Weather Normal.");
+// }
+
+// QUESTION NO 9:
+// var num1 = +prompt("Enter Number 1:");
+// var operator = prompt("Enter Operator (+ - * / %):");
+// var num2 = +prompt("Enter Number 2:");
+
+// if(operator === "+"){
+//     console.log("Answer is " +(num1+num2) );
+// }
+// else if(operator === "-"){
+//     console.log("Answer is " +(num1-num2) );
+// }
+// else if(operator === "*"){
+//     console.log("Answer is " +(num1*num2) );
+// }
+// else if(operator === "/"){
+//     console.log("Answer is " +(num1/num2) );
+// }
+// else if(operator === "%"){
+//     console.log("Answer is " +(num1%num2) );
+// }
